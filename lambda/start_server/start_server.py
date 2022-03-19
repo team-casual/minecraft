a7fbfc4453d22094ee567a1b8a64c0d374a16bfb
werzl <adam@houseofhewitt.co.uk>
2022-03-19 22:06:33 +0000

    update startServer with documentation

--- lambda/start_server/start_server.py
+++ lambda/start_server/start_server.py
@@ -4,12 +4,16 @@ import json
 import boto3
 import time
 
+
 def get_tag_from_instance(instance, key: str):
     for tag in instance.tags:
         if tag['Key'] == key:
             return tag['Value']
 
+
 """
+start_server
+
 Requires the following python layers:
 - discord_layer
     Contains the discord_webhook lib and its dependencies.
@@ -23,11 +27,13 @@ The event value 'queryStringParameters' is retrieved from API Gateway.
 Timeout needs to be increased to allow the loop to run as sometimes 
 instances can take a few minutes to spin up.
 """
+
+
 def lambda_handler(event, context):
     region = "eu-west-2"
     instance_id = event["queryStringParameters"]['instanceId']
     discord_webhook_url = os.environ["discord_webhook_url"]
-    
+
     ec2_resource = boto3.resource('ec2', region_name=region)
 
     try:
@@ -42,16 +48,18 @@ def lambda_handler(event, context):
         if not instance_ip:
             raise ValueError('Server IP address was empty')
 
-        discord_content = f"The Vanilla Minecraft server: **{get_tag_from_instance(instance, 'Name')}** has been started.\nIP address - *{instance_ip}*."
-        webhook = DiscordWebhook(url=discord_webhook_url, content=discord_content)
+        discord_content = f"The Minecraft server: **{get_tag_from_instance(instance, 'Name')}** has been started.\nIP address - *{instance_ip}*."
+        webhook = DiscordWebhook(
+            url=discord_webhook_url, content=discord_content)
         webhook.execute()
 
         return {
             'statusCode': 200,
             'body': json.dumps({'running': True}),
             'headers': {
-                "Access-Control-Allow-Origin" : "*",
-                "Access-Control-Allow-Credentials" : True
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET'
             }
         }
     except Exception as e:
@@ -61,7 +69,8 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({'error': 'unable to start server'}),
             'headers': {
-                "Access-Control-Allow-Origin" : "*",
-                "Access-Control-Allow-Credentials" : True
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET'
             }
         }
